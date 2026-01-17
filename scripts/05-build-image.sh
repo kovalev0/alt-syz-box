81e8dadb50c1d91172ca0a703f91d5be68046946
Vasiliy Kovalev <kovalev@altlinux.org>
2026-01-17 15:45:57 +0300

    scripts/05-build-image.sh: Add gcc package for C repro generation

--- scripts/05-build-image.sh
+++ scripts/05-build-image.sh
@@ -53,6 +53,8 @@ if [ ! -f "$IMAGE_PATH" ]; then
     echo "Downloading guest image..."
     wget --no-check-certificate --progress=dot:mega "$IMAGE_BASE_URL/$IMAGE_FILENAME.xz"
     unxz "./$IMAGE_FILENAME.xz"
+    # Needed for C repro generation
+    PACKAGES_TO_INSTALL="gcc"
 else
     echo "Guest image $IMAGE_PATH already exists. Skipping download."
 fi
