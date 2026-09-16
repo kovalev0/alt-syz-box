811aa5645093e02cdf2a9fe558752d45746cd23b
Vasiliy Kovalev <kovalev@altlinux.org>
2026-09-16 21:21:22 +0300

    kernel: 6.1.180-alt1

--- scripts/02-build-kernel.sh
+++ scripts/02-build-kernel.sh
@@ -58,7 +58,7 @@ git config --global http.sslVerify false
 if [ ! -d "$KERNEL_DIR" ]; then
     mkdir -p "$KERNEL_DIR"
     echo "Cloning kernel from $KERNEL_GIT_URL (branch: $KERNEL_GIT_TAG)..."
-    git clone --branch="$KERNEL_GIT_TAG" "$KERNEL_GIT_URL" "$KERNEL_DIR"
+    git clone --depth=1 --branch="$KERNEL_GIT_TAG" "$KERNEL_GIT_URL" "$KERNEL_DIR"
 
     if ls "$PATCH_DIR"/*.patch.applied 1> /dev/null 2>&1; then
         rename .applied "" "$PATCH_DIR"/*.patch.applied
@@ -96,8 +96,9 @@ make ARCH=x86_64 O="$KERNEL_BUILD_DIR" x86_64_defconfig
 #----------------------------------------------------
 # Apply ALT config
 # https://packages.altlinux.org/ru/c10f2/srpms/kernel-image-6.12/
-# curl -s https://git.altlinux.org/tasks/archive/done/_391/400922/build/100/x86_64/rpms/kernel-image-6.12-6.12.59-alt0.c10f.2.x86_64.rpm | rpm2cpio | cpio -imdv "./boot/config-6.12.59-6.12-alt0.c10f.2"
-cat "$CONTAINER_REPO_DIR/config/kernel/config-6.12.59-6.12-alt0.c10f.2" >> "$KERNEL_BUILD_DIR/.config"
+# https://beta.packages.altlinux.org/ru/p10/srpms/kernel-image-un-def/
+# curl -s https://git.altlinux.org/tasks/archive/done/_417/427587/build/100/x86_64/rpms/kernel-image-un-def-6.1.180-alt1.x86_64.rpm | rpm2cpio | cpio -imdv "./boot/config-6.1.180-un-def-alt1"
+cat "$CONTAINER_REPO_DIR/config/kernel/config-6.1.180-un-def-alt1" >> "$KERNEL_BUILD_DIR/.config"
 
 # Sets the necessary baseline configuration options (including those required to
 # boot without an initrd) before the final configuration or manual overrides are applied.
